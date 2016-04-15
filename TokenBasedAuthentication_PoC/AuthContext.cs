4540254730899f4133dc34fcc9c3465b81536341
guerreroleond <guerreroleond@gmail.com>
2016-04-14 19:11:43 -0500

    First upload from VS

---
+++ TokenBasedAuthentication_PoC/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenBasedAuthentication_PoC
+{
+    public class AuthContext : IdentityDbContext
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+        }
+    }
+}
---
+++ TokenBasedAuthentication_PoC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TokenBasedAuthentication_PoC.WebApiApplication" Language="C#" %>
